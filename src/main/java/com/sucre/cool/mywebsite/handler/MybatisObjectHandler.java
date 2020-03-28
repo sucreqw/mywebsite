0b9544570cf3dd47537c882144bd6f3a32da290e
sucre <sucre@ylbh.com>
2020-03-28 18:17:06 +0800

    增加mybatis-plus 自动填充字段 拦截器

---
+++ src/main/java/com/sucre/cool/mywebsite/handler/MybatisObjectHandler.java
@@ -0,0 +1,28 @@
+package com.sucre.cool.mywebsite.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+
+@Component
+public class MybatisObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        LocalDateTime date = LocalDateTime.now();
+        /*//新增时填充的字段
+        metaObject.setValue("createUser",  UserUtil.getUserId());
+        metaObject.setValue("createDate",  date);
+
+        metaObject.setValue("updateUser",  UserUtil.getUserId());
+        metaObject.setValue("updateDate",  date);*/
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        //更新时 需要填充字段
+      /*  metaObject.setValue("updateUser",  UserUtil.getUserId());
+        metaObject.setValue("updateDate",  LocalDateTime.now());*/
+    }
+}
